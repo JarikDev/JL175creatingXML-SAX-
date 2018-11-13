e8c1b539251f5a2761ad543603e1e3203c978c2a
ivan4000 <sxln1@mail.ru>
2018-11-13 17:34:53 +0300

    JL175creatingXML(SAX)

---
+++ src/com/Main.java
@@ -0,0 +1,65 @@
+package com;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+        XMLOutputFactory factory=XMLOutputFactory.newFactory();
+        XMLStreamWriter writer=factory.createXMLStreamWriter(new FileOutputStream("file.xml"));
+        writer.writeStartDocument();
+        writer.writeStartElement("root");
+        writer.writeStartElement("font");
+        writer.writeAttribute("size","20");
+        writer.writeCharacters("TimesNewRoman");
+        writer.writeEndElement();
+        writer.writeEndElement();
+        writer.writeEndDocument();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
